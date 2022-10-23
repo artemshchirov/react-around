7178c0d12e5b7203f74590ccf97f494d3c0c088a
Artem Shchirov <artemshchirov@gmail.com>
2022-10-23 20:24:43 +0300

    Updates

--- src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="footer section page__footer">
-      <p className="footer__copyright">&copy; 2022 Ɐrtem Schirov</p>
-    </footer>
-  );
-}
